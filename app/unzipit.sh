e178b19fbdbb40212e7d915d4efe33e1bb76c3fe
michaelcunningham <napacunningham@gmail.com>
2024-09-03 10:43:33 -0700

    changes to run on new version of mint

--- app/unzipit.sh
+++ app/unzipit.sh
@@ -7,6 +7,6 @@ this_list=$(find $this_dir -name "*.gz")
 for this_file in $this_list
 do
   echo "Zipping file: $this_file"
-  gunzip $this_file
+  gunzip -k $this_file
 done
 
---
+++ install/make_initial_mint_workstation.sh
@@ -0,0 +1,51 @@
+# First thing is to add the following line to sudoers file
+# Change visudo editor with:
+#    sudo update-alternatives --config editor
+#
+# sudu visudo - and add the following line
+# mcunningham ALL=(root) NOPASSWD: ALL
+
+# echo "set t_ti= t_te=" > ~/.vimrc
+
+# sudo umount -a -t vboxsf
+# sudo chown mcunningham:mcunningham /mnt/install
+# sudo chown mcunningham.mcunningham /mnt/dba
+# sudo chown mcunningham:mcunningham /mnt/vmshare
+
+# sudo mount -t vboxsf -o uid=1000,gid=1000 vd_dba /mnt/dba
+# sudo mount -t vboxsf -o uid=1000,gid=1000 vd_vmshare /mnt/vmshare
+# sudo mount -t vboxsf -o uid=1000,gid=1000 vd_install /mnt/install
+
+# mkdir ~/install
+# cd ~/install
+# ln -s ~/repos/mcunningham/install/make_initial_mint_workstation.sh ~/install/make_initial_mint_workstation.sh
+
+# cp /mnt/vmshare/virtualbox/.pgpass /home/mcunningham/.pgpass
+# chmod 600 /home/mcunningham/.pgpass
+# cp /mnt/vmshare/virtualbox/.bashrc_new_mint_machine /home/mcunningham/.bashrc
+# source /home/mcunningham/.bashrc
+
+# sudo apt install git -y
+# mkdir ~/repos
+# cd ~/repos
+# git clone git@github.com:michaelcunningham/mcunningham.git
+
+# sudo apt install mariadb-client-core -y
+sudo apt install postgresql-client -y
+sudo apt install screen -y
+sudo apt install dos2unix -y
+sudo apt install python3-pip -y
+sudo apt install python3-boto3 -y
+sudo apt install python3-botocore -y   # this will be installed with python3-boto3
+pip3 install duckdb --break-system-packages
+
+cd /mnt/install
+sudo ./aws/install --update
+mkdir -p ~/.aws
+cp -rp /mnt/vmshare/virtualbox/.aws/* ~/.aws
+
+# This is for sqlplus
+cd /usr/lib/x86_64-linux-gnu/
+sudo ln -s libaio.so.1t64 libaio.so.1
+
+
